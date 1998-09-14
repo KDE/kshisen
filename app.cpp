30e462cdbf0693284d7d2de322f1e76cef418040
Stephan Kulow <coolo@kde.org>
1998-09-14 20:45:14 +0000

    I know, Ok is ok too, but OK is more OK some GUI guides say :)
    svn path=/trunk/kdegames/kshisen/; revision=10430

--- app.cpp
+++ app.cpp
@@ -454,7 +454,7 @@ QString App::getPlayerName() {
   e->setText("");
   e->setFocus();
 
-  QPushButton *b = new QPushButton("Ok", dlg);
+  QPushButton *b = new QPushButton("OK", dlg);
   b->setDefault(TRUE);
   if(style() == MotifStyle)
     b->setFixedSize(b->sizeHint().width() + 10,
