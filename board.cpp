c03b729862998958fc6d68e1779717112142d362
Stephan Kulow <coolo@kde.org>
1999-03-02 15:56:50 +0000

    CVS_SILENT replacing klocale->translate with i18n
    svn path=/trunk/kdegames/kshisen/; revision=17436

--- board.cpp
+++ board.cpp
@@ -219,8 +219,8 @@ bool Board::loadTiles(float scale) {
 
   QPixmap pm((PICDIR + "/kshisen.xpm").data());
   if(pm.width() == 0 || pm.height() == 0) {
-    KMsgBox::message(0, klocale->translate("Shisen-Sho"), 
-		     klocale->translate("Cannot load pixmaps!"));
+    KMsgBox::message(0, i18n("Shisen-Sho"), 
+		     i18n("Cannot load pixmaps!"));
     exit(1);
   }
   
