f0a47595cb1170a7658658e38ff39a4cc707db9a
Dave Corrie <kde@davecorrie.com>
2002-07-09 01:20:14 +0000

    Removed spurious letter 'i' :-)
    svn path=/trunk/kdegames/kshisen/; revision=165659

--- app.cpp
+++ app.cpp
@@ -386,7 +386,7 @@ void App::updateScore() {
 QString App::getPlayerName() {
   QDialog *dlg = new QDialog(this, "Hall Of Fame", TRUE);
 
-  QLabel  *l1  = new QLabel(i18n("You've made it into the \"Hall Of Fame\".i Type in\nyour name so mankind will always remember\nyour cool rating."), dlg);
+  QLabel  *l1  = new QLabel(i18n("You've made it into the \"Hall Of Fame\". Type in\nyour name so mankind will always remember\nyour cool rating."), dlg);
   l1->setFixedSize(l1->sizeHint());
 
   QLabel *l2 = new QLabel(i18n("Your name:"), dlg);
