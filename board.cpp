e727c29069c351876f3541feb140e17be2d7fd1e
Dave Corrie <kde@davecorrie.com>
2003-05-22 22:08:06 +0000

    You cannot have a marked tile at the same time as a highlighted tile. Because we have just tested positive for a highlighted tile, mark_x and mark_y are both guaranteed to be -1. Therefore, this code serves no use whatsoever (and so I'm removing it)!
    svn path=/trunk/kdegames/kshisen/; revision=227347

--- board.cpp
+++ board.cpp
@@ -193,11 +193,6 @@ void Board::mousePressEvent(QMouseEvent *e)
 		//kdDebug() << "...left button" << endl;
 		if(highlighted_tile != -1)
 		{
-			int oldmarkx = mark_x;
-			int oldmarky = mark_y;
-			mark_x=-1;
-			mark_y=-1;
-
 			int old_highlighted = highlighted_tile;
 			highlighted_tile = -1;
 
@@ -205,9 +200,6 @@ void Board::mousePressEvent(QMouseEvent *e)
 				for(int j = 0; j < y_tiles(); j++)
 					if(old_highlighted == getField(i, j) && !(i == pos_x && j == pos_y))
 						updateField(i, j, false);
-
-			mark_x = oldmarkx;
-			mark_y = oldmarky;   // no tile selected
 		}
 
 		if(pos_x >= 0 && pos_x < x_tiles() && pos_y >= 0 && pos_y < y_tiles())
