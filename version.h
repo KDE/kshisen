9a68e0a517e646ff27f1d71631af598ba86a2938
Dave Corrie <kde@davecorrie.com>
2003-05-23 14:40:58 +0000

    bump version number
    svn path=/branches/KDE_3_1_BRANCH/kdegames/kshisen/; revision=227437

--- version.h
+++ version.h
@@ -1,1 +1,14 @@
-#define KSHISEN_VERSION	"1.4a"
+// Explanation of KShisen version numbering system
+//
+// Version numbers are of the form: MAJOR.MINOR.MICRO
+//
+// The MAJOR number should only be incremented when really major
+// changes occur. Such an event is not currently foreseeable.
+//
+// The MINOR number is increased for every branch of KDE from HEAD
+// (i.e. it's increased for the KDE3.1 branch, KDE3.2 branch etc.).
+//
+// The MICRO version is increased for every bug-fix to a branch.
+// MICRO numbers >= 90 are used for development milestones in HEAD.
+
+#define KSHISEN_VERSION	"1.4.2"
