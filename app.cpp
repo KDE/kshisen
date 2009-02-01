9b75dadb576a6e1c0a1c1dab1d6df42a570bc265
Frederik Schwarzer <schwarzer@kde.org>
2009-02-01 21:29:00 +0000

    fix pause button not being activated
    svn path=/trunk/KDE/kdegames/kshisen/; revision=919926

--- app.cpp
+++ app.cpp
@@ -284,6 +284,7 @@ void App::updateItems()
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Undo))->setEnabled(m_board->canUndo());
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Redo))->setEnabled(m_board->canRedo());
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Restart))->setEnabled(m_board->canUndo());
+        actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Pause))->setEnabled(true);
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Hint))->setEnabled(true);
     }
 }
