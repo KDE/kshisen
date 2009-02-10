cf6ebce21c0d6c516b25447c70e4cd4de4dacaff
Frederik Schwarzer <schwarzer@kde.org>
2009-02-10 15:54:44 +0000

    change licence header
    use the GPL one from http://techbase.kde.org/Policies/Licensing_Policy
    keeping the license and copyright holders as is of course.
    
    svn path=/trunk/KDE/kdegames/kshisen/; revision=924369

--- src/app.h
+++ src/app.h
@@ -1,42 +1,22 @@
-/* Yo Emacs, this is -*- C++ -*-
- *******************************************************************
- *******************************************************************
- *
- *
- * KSHISEN
- *
- *
- *******************************************************************
- *
- * A japanese game similar to mahjongg
- *
- *******************************************************************
- *
- * Copyright (C)  1997 by Mario Weilguni <mweilguni@sime.com>
- * Copyright (C) 2002-2004 Dave Corrie  <kde@davecorrie.com>
- *
- *******************************************************************
- *
- * This file is part of the KDE project "KSHISEN"
- *
- * KSHISEN is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * KSHISEN is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with KSHISEN; see the file COPYING.  If not, write to
- * the Free Software Foundation, 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- *
- *******************************************************************
- */
-
+/***************************************************************************
+ *   KShisen - A japanese game similar to mahjongg                         *
+ *   Copyright (C) 1997  Mario Weilguni <mweilguni@sime.com>               *
+ *   Copyright (C) 2002-2004  Dave Corrie <kde@davecorrie.com>             *
+ *   Copyright (C) 2009  Frederik Schwarzer <schwarzerf@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
 
 #ifndef APP_H
 #define APP_H
