14c6de5bdbec3bc86677a1f2f61ee8c71985f802
Pino Toscano <pino@kde.org>
2009-04-20 13:04:21 +0000

    don't forget to extract .ui, .kcfg and .rc files
    svn path=/trunk/KDE/kdegames/kshisen/; revision=956633

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-$XGETTEXT src/*.cpp -o $podir/kshisen.pot
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT src/*.cpp rc.cpp -o $podir/kshisen.pot
+rm -f rc.cpp
