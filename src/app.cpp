3b8633b4febea6c94f9530f90ff44b9f332bc6fe
Frederik Schwarzer <schwarzer@kde.org>
2009-05-08 21:20:07 +0000

    unpause the game if the player requests a hint
    There is a problem with the current pause-on-focus-loss behaviour.
    If the player opens the menu to request a hint, the hint action is
    disabled due to the pause mode.
    I now keep the hint action enabled in pause mode and unpause the game
    automatically if the player requests a hint.
    
    svn path=/trunk/KDE/kdegames/kshisen/; revision=965416

--- src/app.cpp
+++ src/app.cpp
@@ -222,6 +222,11 @@ void App::redo()
  */
 void App::hint()
 {
+// The game pauses as soon as the player opens the menu to receive a hint.
+// This is an ugly hack to make the behaviour less crappy (although it still is).
+if (m_board->isPaused()) {
+    m_board->setPauseEnabled(false);
+}
 #ifdef DEBUGGING
     m_board->makeHintMove();
 #else
@@ -246,7 +251,6 @@ void App::updateItems()
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Undo))->setEnabled(false);
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Redo))->setEnabled(false);
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Restart))->setEnabled(false);
-        actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Hint))->setEnabled(false);
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Pause))->setChecked(true);
     } else {
         actionCollection()->action(KStandardGameAction::name(KStandardGameAction::Undo))->setEnabled(m_board->canUndo());
