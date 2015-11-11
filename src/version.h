9c8745592c84adaa04959eb0c6ff051771d13f7f
Frederik Schwarzer <schwarzer@kde.org>
2015-11-11 18:49:16 +0100

    Make Krazy happy.

--- src/version.h
+++ src/version.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   KShisen - A japanese game similar to mahjongg                         *
+ *   Copyright 2010  Frederik Schwarzer <schwarzer@kde.org>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
 // Version numbers are of the form: MAJOR.MINOR.MICRO
 // The MAJOR number should only be incremented when really major changes occur.
 // The MINOR number is increased to a release if new features were added.
