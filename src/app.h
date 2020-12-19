606f2ead9333a010d9a23140615ba314bd8c13ae
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-19 10:42:52 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification.
    
    GIT_SILENT

--- src/app.h
+++ src/app.h
@@ -1,22 +1,11 @@
-/***************************************************************************
- *   KShisen - A japanese game similar to mahjongg                         *
- *   Copyright 1997  Mario Weilguni <mweilguni@sime.com>                   *
- *   Copyright 2002-2004  Dave Corrie <kde@davecorrie.com>                 *
- *   Copyright 2009,2016  Frederik Schwarzer <schwarzer@kde.org>           *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- ***************************************************************************/
+/*
+    KShisen - A japanese game similar to Mahjongg
+    SPDX-FileCopyrightText: 1997 Mario Weilguni <mweilguni@sime.com>
+    SPDX-FileCopyrightText: 2002-2004 Dave Corrie <kde@davecorrie.com>
+    SPDX-FileCopyrightText: 2009, 2016 Frederik Schwarzer <schwarzer@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef KSHISEN_APP_H
 #define KSHISEN_APP_H
--- src/board.h
+++ src/board.h
@@ -1,23 +1,12 @@
-/***************************************************************************
- *   KShisen - A japanese game similar to mahjongg                         *
- *   Copyright 1997  Mario Weilguni <mweilguni@sime.com>                   *
- *   Copyright 2002-2004  Dave Corrie <kde@davecorrie.com>                 *
- *   Copyright 2007  Mauricio Piacentini <mauricio@tabuleiro.com>          *
- *   Copyright 2009-2016  Frederik Schwarzer <schwarzer@kde.org>           *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- ***************************************************************************/
+/*
+    KShisen - A japanese game similar to Mahjongg
+    SPDX-FileCopyrightText: 1997 Mario Weilguni <mweilguni@sime.com>
+    SPDX-FileCopyrightText: 2002-2004 Dave Corrie <kde@davecorrie.com>
+    SPDX-FileCopyrightText: 2007 Mauricio Piacentini <mauricio@tabuleiro.com>
+    SPDX-FileCopyrightText: 2009-2016 Frederik Schwarzer <schwarzer@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 // KMahjonggLib integration and SVG support for KDE 4: Mauricio Piacentini <mauricio@tabuleiro.com>
 
