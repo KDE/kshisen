3128b16aa014e397bab5d4a76641fb68028432b9
Friedrich W. H. Kossebau <kossebau@kde.org>
2024-01-16 02:18:29 +0100

    Use more QByteArrayLiteral
    GIT_SILENT

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char ** argv)
 {
     QApplication a(argc, argv);
 
-    KLocalizedString::setApplicationDomain("kshisen");
+    KLocalizedString::setApplicationDomain(QByteArrayLiteral("kshisen"));
 
     KAboutData aboutData(QStringLiteral("kshisen"), i18n("Shisen-Sho"),
                          QStringLiteral(KSHISEN_VERSION_STRING),
